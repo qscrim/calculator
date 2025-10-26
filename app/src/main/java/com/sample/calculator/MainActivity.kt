cc270b30907cff9a4744047c9454206c5f73a934
qscrim <tochnonemaltsev@yandex.ru>
2025-10-26 15:05:24 +0700

    Добавление слайдера для выбора процента чаевых

--- app/src/main/java/com/sample/calculator/MainActivity.kt
+++ app/src/main/java/com/sample/calculator/MainActivity.kt
@@ -7,10 +7,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -20,6 +22,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -46,6 +49,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     var orderAmount by remember { mutableStateOf("") }
     var dishCount by remember { mutableStateOf("") }
+    var tipPercentage by remember { mutableStateOf(0f) }
 
     Column(
         modifier = modifier.padding(16.dp)
@@ -93,6 +97,44 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                 },
                 singleLine = true,
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+        }
+
+        Text(
+            text = "Чаевые:",
+            fontSize = 18.sp,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+        Slider(
+            value = tipPercentage,
+            onValueChange = { newValue -> tipPercentage = newValue },
+            valueRange = 0f..25f,
+            steps = 24,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 8.dp)
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp)
+        ) {
+            Text(
+                text = "0%",
+                fontSize = 14.sp,
+                modifier = Modifier.weight(1f)
+            )
+
+            Text(
+                text = "Текущий процент: ${tipPercentage.toInt()}%",
+                fontSize = 14.sp
+            )
+
+            Text(
+                text = "25%",
+                fontSize = 14.sp,
+                modifier = Modifier.weight(1f),
+                textAlign = TextAlign.End
             )
         }
     }
